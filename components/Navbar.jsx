81e5d115b26adb9803a9dfe423fb77816e8e2099
Liliana <liliana.solorzano@hotmail.com>
2023-06-03 11:54:12 -0600

    Front-end

---
+++ components/Navbar.jsx
@@ -0,0 +1,55 @@
+'use client';
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import { navVariants } from '../utils/motion';
+import { useState } from 'react';
+import RegistrationForm from './RegistrationForm';
+
+const Navbar = () => {
+  const [showRegistrationForm, setShowRegistrationForm] = useState(false);
+
+  const handleRegistrationClick = () => {
+    setShowRegistrationForm(true);
+  };
+
+  return (
+    <motion.nav
+      variants={navVariants}
+      initial="hidden"
+      whileInView="show"
+      className={`${styles.xPaddings} py-8 relative`}
+    >
+      <div className="absolute w-[50%] inset-0 gradient-01" />
+      <div
+        className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}
+      >
+        <img
+          src="/search.svg"
+          alt="search"
+          className="w-[24px] h-[24px] object-contain"
+        />
+        <h2
+          className="font-extrabold text-[24px] leading-[30.24px] text-white"
+          style={{ marginLeft: '180px', marginTop: '20px' }}
+        >
+          SOLANA CROWDFUNDING
+        </h2>
+        {showRegistrationForm ? (
+          <RegistrationForm />
+        ) : (
+          <button
+            type="button"
+            className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]"
+            onClick={handleRegistrationClick}
+          >
+            <span className="font-normal text-[16px] text-white">
+              Register
+            </span>
+          </button>
+        )}
+      </div>
+    </motion.nav>
+  );
+};
+
+export default Navbar;
---
+++ sections/About.jsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import { TypingText } from '../components';
+
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
+const About = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText title="| About Solana" textStyles="text-center" />
+
+      <motion.p
+        variants={fadeIn('up', 'tween', 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+      >
+        <span className="font-extrabold text-white">Solana's</span> high-performance blockchain technology enables rapid transaction processing, allowing for near-instantaneous crowdfunding activities. 
+        This speed ensures that projects can quickly secure funding, 
+        enabling faster implementation and execution of ideas.{' '}
+        <span className="font-extrabold text-white">
+        Solana Crowdfunding 
+        </span>{' '}
+        eliminates geographical barriers, enabling global participation. Anyone with an internet connection can contribute to projects and support initiatives, fostering a more inclusive and diverse crowdfunding ecosystem.
+      </motion.p>
+
+      <motion.img
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        src="/arrow-down.svg"
+        alt="arrow down"
+        className="w-[18px] h-[28px] object-contain mt-[28px]"
+      />
+    </motion.div>
+  </section>
+);
+
+export default About;
