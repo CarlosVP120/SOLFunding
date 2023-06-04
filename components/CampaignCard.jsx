20d8852d64ebd56f28529cc718be730fe46a5c1b
Diego <diegopartidaromero@gmail.com>
2023-06-04 02:06:21 -0600

    Add Campaign Cards to the page

--- components/CampaignCard.jsx
+++ components/CampaignCard.jsx
@@ -121,8 +121,7 @@ const CampaignCard = () => {
   ];
 
   return (
-    <div className="container mx-auto py-8">
-      <h1 className="text-3xl font-bold mb-4">Campaigns</h1>
+    <div className=" mx-auto py-8">
       <CampaignsList campaigns={fakeCampaigns} />
     </div>
   );
